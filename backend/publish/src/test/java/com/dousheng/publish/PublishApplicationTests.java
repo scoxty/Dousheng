ec9285c07295db1e4484c709b8c98da04cb28ffa
scoxty <14464961+scoxty@user.noreply.gitee.com>
2025-04-05 22:53:40 +0800

    feat: 初始化后端项目

---
+++ backend/publish/src/test/java/com/dousheng/publish/PublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dousheng.publish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
