3a4eda65ec5c956d032635c1996f15b3174f565c
scoxty <14464961+scoxty@user.noreply.gitee.com>
2025-04-07 23:06:11 +0800

    feat: 新增服务启动成功的输出

--- backend/auth/src/main/java/com/dousheng/auth/AuthApplication.java
+++ backend/auth/src/main/java/com/dousheng/auth/AuthApplication.java
@@ -10,5 +10,6 @@ public class AuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
+        System.out.println("AuthApplication started");
     }
 }
