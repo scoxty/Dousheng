e3409ea7c6d199a616f0a616746c2b0448aa7911
scoxty <14464961+scoxty@user.noreply.gitee.com>
2025-04-12 00:00:10 +0800

    feat: 补充dubbo注解以及mock返回

--- backend/comment/src/main/java/com/dousheng/comment/handler/CommentRpcServiceHandler.java
+++ backend/comment/src/main/java/com/dousheng/comment/handler/CommentRpcServiceHandler.java
@@ -3,10 +3,13 @@ package com.dousheng.comment.handler;
 import com.dousheng.dto.req.comment.GetVideoCommentCountsReqDTO;
 import com.dousheng.dto.resp.comment.GetVideoCommentCountsRespDTO;
 import com.dousheng.service.CommentRpcService;
+import org.apache.dubbo.config.annotation.DubboService;
 
+@DubboService
 public class CommentRpcServiceHandler implements CommentRpcService {
     @Override
     public GetVideoCommentCountsRespDTO getVideoCommentCounts(GetVideoCommentCountsReqDTO requestParam) {
-        return null;
+        GetVideoCommentCountsRespDTO respDTO = GetVideoCommentCountsRespDTO.builder().code("0").message("success").commentCounts(0).build();
+        return respDTO;
     }
 }
