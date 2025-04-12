3b22f53ea922e8add76a647f319fb6eb7dd3c94c
scoxty <14464961+scoxty@user.noreply.gitee.com>
2025-04-13 00:18:45 +0800

    update: 使用TimeUnit转换时间

--- backend/user/src/main/java/com/dousheng/user/common/constant/RedisCacheConstant.java
+++ backend/user/src/main/java/com/dousheng/user/common/constant/RedisCacheConstant.java
@@ -1,12 +1,14 @@
 package com.dousheng.user.common.constant;
 
+import java.util.concurrent.TimeUnit;
+
 public class RedisCacheConstant {
     public static final String LOCK_GET_USERID_BY_NAME_KEY = "dousheng:lock_get-userid-by-name:%s";
     public static final String LOCK_GET_USERINFO_BY_ID_KEY = "dousheng:lock_get-userinfo-by-id:%d";
 
     public static final String USER_INFO_KEY = "dousheng:user-info-by-id:%d";
-    public static final long USER_INFO_BASE_TTL = 60 * 60 * 24 * 7; // 7 天
+    public static final long USER_INFO_BASE_TTL = TimeUnit.DAYS.toSeconds(7); // 7 天
 
     public static final String USER_ID_KEY = "dousheng:user-id-by-name:%s";
-    public static final long USER_ID_BASE_TTL = 60 * 60 * 24 * 7; // 7 天
+    public static final long USER_ID_BASE_TTL = TimeUnit.DAYS.toSeconds(7); // 7 天
 }
